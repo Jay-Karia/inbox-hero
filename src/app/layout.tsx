53a8f46215ce5ca2cb9aa467a8624215380d9965
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-06-24 20:25:51 +0530

    chore: add google clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,13 @@ import type { Metadata } from "next";
 import {
   ClerkProvider,
   SignInButton,
-  SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import {Button} from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,8 +40,9 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
-              <SignUpButton />
+              <Button asChild>
+                <SignInButton mode="modal"/>
+              </Button>
             </SignedOut>
             <SignedIn>
               <UserButton />
