6c86b1d19ee1b27219482bbdcbe8dd1effd1f6b9
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-06-26 14:37:20 +0530

    chore: updated gradient glow colors

--- src/components/ui/background-gradient.tsx
+++ src/components/ui/background-gradient.tsx
@@ -6,10 +6,14 @@ export const BackgroundGradient = ({
   gradientBackgroundStart = "rgb(108, 0, 162)",
   gradientBackgroundEnd = "rgb(0, 17, 82)",
   thirdColor = "0, 197, 255",
+  fourthColor = "255, 0, 255", // Bright magenta
   fifthColor = "255, 255, 3",
+  sixthColor = "0, 255, 127", // Bright green
+  seventhColor = "255, 165, 0", // Orange
+  eighthColor = "147, 0, 211", // Purple
   pointerColor = "140, 100, 255",
   size = "85%",
-  blendingValue = "hard-light",
+  blendingValue = "screen", // Changed to screen for brighter glow
   children,
   className,
   containerClassName,
@@ -21,6 +25,9 @@ export const BackgroundGradient = ({
   thirdColor?: string;
   fourthColor?: string;
   fifthColor?: string;
+  sixthColor?: string;
+  seventhColor?: string;
+  eighthColor?: string;
   pointerColor?: string;
   size?: string;
   blendingValue?: string;
@@ -39,17 +46,25 @@ export const BackgroundGradient = ({
       gradientBackgroundEnd
     );
     document.body.style.setProperty("--third-color", thirdColor);
+    document.body.style.setProperty("--fourth-color", fourthColor);
     document.body.style.setProperty("--fifth-color", fifthColor);
+    document.body.style.setProperty("--sixth-color", sixthColor);
+    document.body.style.setProperty("--seventh-color", seventhColor);
+    document.body.style.setProperty("--eighth-color", eighthColor);
     document.body.style.setProperty("--pointer-color", pointerColor);
     document.body.style.setProperty("--size", size);
     document.body.style.setProperty("--blending-value", blendingValue);
   }, [
     blendingValue,
     fifthColor,
+    fourthColor,
     gradientBackgroundEnd,
     gradientBackgroundStart,
     pointerColor,
     size,
+    sixthColor,
+    seventhColor,
+    eighthColor,
     thirdColor,
   ]);
 
@@ -96,31 +111,32 @@ export const BackgroundGradient = ({
       >
         <div
           className={cn(
-            `absolute [background:radial-gradient(circle_at_center,_rgba(var(--third-color),_0.8)_0,_rgba(var(--third-color),_0)_50%)_no-repeat]`,
+            `absolute [background:radial-gradient(circle_at_center,_rgba(var(--third-color),_0.9)_0,_rgba(var(--third-color),_0)_55%)_no-repeat]`,
             `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(100%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
             `[transform-origin:calc(50%+400px)]`,
-            `animate-third`,
-            `opacity-100 overflow-visible`
+            `animate-third animate-glow`,
+            `opacity-90`
           )}
         ></div>
         <div
           className={cn(
-            `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fifth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_45%)_no-repeat]`,
-            `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] border top-[calc(100%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+            `absolute [background:radial-gradient(circle_at_center,_rgba(var(--sixth-color),_0.9)_0,_rgba(var(--fifth-color),_0)_45%)_no-repeat]`,
+            `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(110%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
             `[transform-origin:calc(50%-800px)_calc(50%+800px)]`,
-            `animate-fifth`,
-            `opacity-50 overflow-visible`
+            `animate-fifth animate-glow`,
+            `opacity-90`
           )}
         ></div>
         <div
           className={cn(
-            `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fifth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_40%)_no-repeat]`,
-            `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(100%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+            `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fourth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_40%)_no-repeat]`,
+            `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(110%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
             `[transform-origin:calc(50%-800px)_calc(50%+800px)]`,
-            `animate-fifth`,
-            `opacity-80 overflow-visible`
+            `animate-fifth animate-glow`,
+            `opacity-80`
           )}
         ></div>
+
       </div>
     </div>
   );
