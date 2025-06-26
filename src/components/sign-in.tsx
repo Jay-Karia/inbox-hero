8f2253196d576b205be0f946474da30dc43da08b
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-06-26 15:26:42 +0530

    fix: sign-in button type error

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,16 +1,29 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 
-export default function SignIn(props: {
-  className?: string;
-  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "glass";
-} = {}
+export default function SignIn(
+  props: {
+    className?: string;
+    variant?:
+      | "default"
+      | "destructive"
+      | "outline"
+      | "secondary"
+      | "ghost"
+      | "link"
+      | "glass";
+  } = {}
 ) {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal" className={props.className}>
-          <Button variant={props.variant ? props.variant : "outline"}>Sign In</Button>
+        <SignInButton mode="modal">
+          <Button
+            className={props.className}
+            variant={props.variant ? props.variant : "outline"}
+          >
+            Sign In
+          </Button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
