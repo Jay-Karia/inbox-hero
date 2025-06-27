05f9a93fe35df303f2255a98fc754bf49f2fbd12
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-06-27 08:25:07 +0530

    chore: connected hono and clerk

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -1,11 +1,27 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 const app = new Hono().basePath('/api')
 
 app.get('/hello', (c) => {
   return c.json({
     message: 'Hello from Hono!'
+  })
+})
+
+app.get("/protected", clerkMiddleware(), (c) => {
+  const auth = getAuth(c)
+
+  if (!auth?.userId) {
+    return c.json({
+      message: 'You are not logged in.',
+    })
+  }
+
+  return c.json({
+    message: 'You are logged in!',
+    userId: auth.userId,
   })
 })
 
